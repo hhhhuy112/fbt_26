4b1491d4bdae8a179c950896ba775e5081a39a1e
Diem Ho <diemho2112@gmail.com>
2018-07-03 10:15:30 +0700

    migration

---
+++ database/migrations/2018_07_03_025005_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('tour_id')->unsigned()->index();
+            $table->integer('number_of_passengers')->unsigned();
+            $table->integer('grand_total')->unsigned();
+            $table->string('status');
+            $table->date('booking_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
---
+++ database/migrations/2018_07_03_025050_create_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('tour_id')->unsigned()->index();
+            $table->string('name');
+            $table->text('desciption');
+            $table->integer('discount')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
